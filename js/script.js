067d037735216a009ab4f5df222b4e96e44cbafa
deepanshu188 <deepanshubisht188@gmail.com>
2022-08-21 20:59:08 +0530

    reduced repetition & made more efficient

--- js/script.js
+++ js/script.js
@@ -1,45 +1,46 @@
-let hour1 = document.getElementsByClassName('hour-1')
-let hour2 = document.getElementsByClassName('hour-2')
-let min1 = document.getElementsByClassName('min-1')
-let min2 = document.getElementsByClassName('min-2')
+// DOM Selectors
+const selector = (name) => document.getElementsByClassName(name);
+
+const hour1 = selector("hour-1");
+const hour2 = selector("hour-2");
+const min1 = selector("min-1");
+const min2 = selector("min-2");
+
+// circles color
+const fill = "#fff";
+const resetColor = "#000";
 
 setInterval(() => {
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    
-    let h = hour.toString()
-    let m = minute.toString()
-    if(h <= 9)
-        h = '0'.concat(h)
-    
-    if(m <= 9)
-        m = '0'.concat(m);
-    
-    let h1 = Number(h[0]).toString(2).split('').reverse().join('')
-    let h2 = Number(h[1]).toString(2).split('').reverse().join('')
-    let m1 = Number(m[0]).toString(2).split('').reverse().join('')
-    let m2 = Number(m[1]).toString(2).split('').reverse().join('')
-    
-    let fill = '#fff';
-    let resetColor = '#000';
-    let j = 3;
-    
-    for(let i = 0; i <= 3; i++) {
-        hour1[j].style.background = resetColor;
-        hour2[j].style.background = resetColor;
-        min1[j].style.background = resetColor;
-        min2[j].style.background = resetColor;
-            
-        if(h1[i] === '1')
-            hour1[j].style.background = fill;
-        if(h2[i] === '1')
-            hour2[j].style.background = fill;
-        if(m1[i] === '1')
-            min1[j].style.background = fill;
-        if(m2[i] === '1')
-            min2[j].style.background = fill;
-        j--;
-    }
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
 
-}, 1000)
+  const h = hour.toString();
+  const m = minute.toString();
+
+  if (h <= 9) h = "0".concat(h);
+
+  if (m <= 9) m = "0".concat(m);
+
+  const toBinary = (val) =>
+    Number(val).toString(2).split("").reverse().join("");
+
+  const h1 = toBinary(h[0]);
+  const h2 = toBinary(h[1]);
+  const m1 = toBinary(m[0]);
+  const m2 = toBinary(m[1]);
+
+  let j = 3;
+
+  for (let i = 0; i <= 3; i++) {
+    const clocks = [hour1, hour2, min1, min2];
+
+    clocks.map((clock) => (clock[j].style.background = resetColor));
+
+    if (h1[i] === "1") hour1[j].style.background = fill;
+    if (h2[i] === "1") hour2[j].style.background = fill;
+    if (m1[i] === "1") min1[j].style.background = fill;
+    if (m2[i] === "1") min2[j].style.background = fill;
+    j--;
+  }
+}, 1000);
